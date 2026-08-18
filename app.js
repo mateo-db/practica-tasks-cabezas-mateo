fee5612e7cd2cd1599c027df892c50527600466a
mateo-db <ipfcabezas@gmail.com>
2026-08-17 22:34:24 -0300

    feat: se importaron rutas y controladores de los nuevos modelos a app.js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ import { User } from './src/models/user.model.js';
 import { Task } from './src/models/task.model.js';
 import userRouter from './src/routes/user.routes.js';
 import taskRouter from './src/routes/task.routes.js';
+import tagRouter from './src/routes/tag.routes.js';
+import profileRouter from './src/routes/profile.routes.js';
+import taskTagRouter from './src/routes/task_tag.routes.js';
 
 //variable con express activado
 const app = express()
@@ -16,8 +19,11 @@ const app = express()
 app.use(express.json())
 
 //activamos los routers
-app.use(userRouter)
-app.use(taskRouter)
+app.use("/api/users", userRouter)
+app.use("/api/tasks", taskRouter)
+app.use("/api/profiles", profileRouter)
+app.use("/api/tags", tagRouter)
+app.use("/api/task_tags", taskTagRouter)
 
 const PORT = process.env.PORT
 
